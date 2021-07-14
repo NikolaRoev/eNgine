8bff5386181e5598fc3472fc6180890dedced901
nikola.roev@gmail.com <nikola.roev@gmail.com>
2021-07-14 23:53:02 +0300

    Some fixes for drag and drop.

--- eNgine/src/UI/UI.cpp
+++ eNgine/src/UI/UI.cpp
@@ -22,12 +22,9 @@ void menu_bar_ui() {
 
 void recursive_node_create(const std::filesystem::path& path) {
 	for (auto& entry : std::filesystem::directory_iterator(path)) {
+		bool open_node = false;
 		if (entry.is_directory()) {
-			if (ImGui::TreeNode(entry.path().filename().string().c_str())) {
-				recursive_node_create(entry.path());
-
-				ImGui::TreePop();
-			}
+			open_node = ImGui::TreeNodeEx(entry.path().filename().string().c_str());
 		}
 		else {
 			if (ImGui::Selectable(entry.path().filename().string().c_str())) {
@@ -35,8 +32,7 @@ void recursive_node_create(const std::filesystem::path& path) {
 			}
 		}
 
-			
-		if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceAllowNullID)) {
+		if (ImGui::BeginDragDropSource()) {
 			ImGui::SetDragDropPayload("PATH", entry.path().string().data(), entry.path().string().size());
 
 			ImGui::Text(entry.path().filename().string().c_str());
@@ -48,10 +44,26 @@ void recursive_node_create(const std::filesystem::path& path) {
 			if (auto payload = ImGui::AcceptDragDropPayload("PATH")) {
 				std::filesystem::path payload_path(std::string{ static_cast<char*>(payload->Data), static_cast<std::string::size_type>(payload->DataSize) });
 
-				//TO DO: Handle if dragdrop source and dragdrop target are either file or directory. Good luck lul.
+				if (entry.is_directory()) {
+					//Add if entry is not directory so we can throw anywhere ye.
+					//Fix so  relative paths don't get fucked.
+					std::filesystem::rename(payload_path, entry.path() / payload_path.filename());
+
+					ImGui::EndDragDropTarget();
+					if (open_node) {
+						ImGui::TreePop();
+					}
+					break;
+				}
 			}
 
 			ImGui::EndDragDropTarget();
+		}
+
+		if (open_node) {
+			recursive_node_create(entry.path());
+
+			ImGui::TreePop();
 		}
 	}
 }
@@ -61,7 +73,7 @@ void recursive_node_create(const std::filesystem::path& path) {
 void resources_ui(ResourceManager& resource_manager, sf::RenderWindow& window) {
 	ImGui::Begin("Resources");
 
-	recursive_node_create("../assets");
+	recursive_node_create("..\\assets");
 
 	ImGui::End();
 }
